9e415ffe2fe5caa11c8cd5ba2b84a95daa70e4b7
wuding <netjoin@gmail.com>
2019-08-19 15:11:56 +0800

    添加虚拟主机配置

--- app/config.php
+++ app/config.php
@@ -2,11 +2,21 @@
 
 return array(
     'virtual_paths' => array(
-        3 => array(
-            '/^[\x7f-\xff]+$/' => ROOT_DIR . '/tmp/www/test.php',
+        '' => array(
+            '/^[a-z\.]{7,12}$/' => 'filename',
+            '/^[a-z]{4}(.*)$/' => 'filename',
         ),
-        7 => array(
-            '/[a-z]{7,7}/' => ROOT_DIR . '/tmp/www/test.php',
+        'filename' => ROOT_DIR . '/www/.dist/a-z7.php',
+        3 => array(
+            '/^[\x7f-\xff]+$/' => ROOT_DIR . '/www/.dist/test.php',
         ),
     ),
+    'virtual_hosts' => array(
+        '' => array(
+            'http_host' => ROOT_DIR . '/www/.dist/http_host.php',
+        ),
+        'yingmi.xyz' => ROOT_DIR . '/www\work\wuding\astrology\web/index.php',
+        'urlnk.cc urlnk.host' => array('http_host'),
+    ),
+    'host_domain' => '.loc.urlnk.com',
 );
