4a318b7812162b6f304b4827b06235a22a1a6ff6
wuding <netjoin@gmail.com>
2019-08-19 16:39:24 +0800

    添加 2 个虚拟主机

--- app/config.php
+++ app/config.php
@@ -14,9 +14,13 @@ return array(
     'virtual_hosts' => array(
         '' => array(
             'http_host' => ROOT_DIR . '/www/.dist/http_host.php',
+            'astro' => ROOT_DIR .'/www\work\wuding\astrology\web\index.php',
+            'mr-fact' => ROOT_DIR .'/www\work\netjoin/mr-fact\web\index.php',
         ),
         'yingmi.xyz' => ROOT_DIR . '/www\work\wuding\astrology\web/index.php',
         'urlnk.cc urlnk.host' => array('http_host'),
+        'robot.urlnk.com' => array('astro'),
+        'api.urlnk.com' => array('mr-fact'),
     ),
     'host_domain' => '.loc.urlnk.com',
 );
