a218207d8b7c15b3e7c9819b03b69cabe7c79ab5
wuding <netjoin@gmail.com>
2019-08-20 21:49:09 +0800

    添加首页多搜功能

--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -6,7 +6,21 @@ class Index extends \MagicCube\Controller
 {
     public function index()
     {
-        # return __FILE__;
-        return ['hello' => 'world', 'nihao'];
+        $arr = array();
+        $arr[] = array(
+            array(100, '百度'),
+            array(191571521, '搜狗'),
+            array(360, '360搜索'),
+        );
+        $arr[] = array(
+            array(900916, 'Google'),
+            array(251, 'Yahoo'),
+            array(0, 'DuckDuckGo'),
+        );
+        $arr[] = array(
+            array(2, '海词'),
+            array(3, '优惠券'),
+        );
+        return ['arr' => $arr];
     }
 }
--- app/index/template/index/index.php
+++ app/index/template/index/index.php
@@ -1,3 +1,32 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>有链网 ^_^</title>
+</head>
 
-print_r(get_defined_vars());
+<body>
+<form id="search_form" action="/s" target="_search">
+    <input type="text" name="q" />
+    <button type="submit">Go</button>
+    <input type="hidden" name="id" id="engine_id">
+</form>
+
+<?php
+$html = '';
+foreach ($arr as $k => $variable) {
+	$p = '<p>' . PHP_EOL;
+	foreach ($variable as $key => $value) {
+		list($id, $name) = $value;
+		$button = "<button type=\"button\" onclick=\"go($id)\">$name</button>";
+		$p .= $button . PHP_EOL;
+	}
+	$p .= '</p>' . PHP_EOL;
+	$html .= $p;
+}
+echo $html;
+?>
+
+<script type="text/javascript" src="js/search.js"></script>
+</body>
+</html>
