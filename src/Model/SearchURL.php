e6037457159df60cd4848cdff53dd8d7614ddd25
wuding <netjoin@gmail.com>
2019-11-28 18:06:37 +0800

    修改数据库名称

--- src/Model/SearchURL.php
+++ src/Model/SearchURL.php
@@ -4,7 +4,7 @@ namespace Model;
 
 class SearchURL extends \Topdb\Table
 {
-    public $db_name = 'url';
+    public $db_name = 'opermonitor';
     public $table_name = 'search';
     public $primary_key = 'search_id';
 }
