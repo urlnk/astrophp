525278c44fab2707a86f93d508f2ba047e2ffb7a
wuding <netjoin@gmail.com>
2019-12-09 18:56:07 +0800

    修复刷卡状态

--- app/ka/controller/Api.php
+++ app/ka/controller/Api.php
@@ -24,12 +24,12 @@ class Api extends _controller
             # $decimal = '507015516';
 
             // 查找用户
-            $sql = "SELECT A.user_id AS user_id, user_name, telephone, operator_name, organ_name, A.operator_id 
+            $sql = "SELECT A.user_id AS user_id, user_name, telephone, operator_name, organ_name, A.operator_id, card_status 
 FROM $this->db.pl_card_t A 
 LEFT JOIN $this->db.pl_user_t B ON B.user_id = A.user_id 
 LEFT JOIN $this->db.pl_operator_info_t C ON C.operator_id = A.operator_id 
 LEFT JOIN $this->db.pl_organization_t D ON D.organ_id = B.organ_id 
-WHERE A.card_code = '$decimal' 
+WHERE A.card_code = '$decimal' AND A.card_status != 'UNRELEASE' 
 LIMIT 50";
             $statement = $SearchURL->query($sql);
             $data = $statement->fetchAll(\PDO::FETCH_OBJ);
---
+++ web/run.bat
@@ -0,0 +1,2 @@
+@echo  off
+start iexplore -k http://127.0.0.1:5002/ka
