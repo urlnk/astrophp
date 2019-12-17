ae06659bb2c8b55eff87fb97cb07b8bbe0b24a8b
wuding <netjoin@gmail.com>
2019-12-17 22:50:58 +0800

    视频全屏隐藏键盘2

--- app/ka/controller/_controller.php
+++ app/ka/controller/_controller.php
@@ -4,7 +4,7 @@ namespace App\Ka\Controller;
 
 class _Controller extends \MagicCube\Controller
 {
-    public $static_version = '?v=350';
+    public $static_version = '?v=351';
 
     public function __construct($vars = [])
     {
