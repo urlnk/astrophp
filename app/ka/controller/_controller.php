a0003666c64e8be8ecb2ebbac0601b3c2f12b0fe
wuding <netjoin@gmail.com>
2019-12-19 02:36:44 +0800

    更新静态文件版本

--- app/ka/controller/_controller.php
+++ app/ka/controller/_controller.php
@@ -4,7 +4,7 @@ namespace App\Ka\Controller;
 
 class _Controller extends \MagicCube\Controller
 {
-    public $static_version = '?v=387';
+    public $static_version = '?v=401';
 
     public function __construct($vars = [])
     {
